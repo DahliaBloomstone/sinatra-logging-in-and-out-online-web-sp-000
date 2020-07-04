0170b81e81c62b2d009384ed4918112057a9b62f
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-04 20:04:35 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_users_table.rb
+++ db/migrate/01_create_users_table.rb
@@ -14,4 +14,4 @@ end
 #create users table to inherit active record
 #table has user username, password, balance, and timestamps
 #To create table: 1. run rake db:create_migration NAME= create_users_table, 2. rename file, 3. create class/class method
-#To add table to schema: run rake db:migrate
+#To add table to schema: run rake db:migrate or rake db:migrate SINATRA_ENV=test if there are issues
