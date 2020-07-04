0e4c17da465196b506b022ac0fc75b1d41afd280
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-04 19:47:25 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,6 +1,7 @@
 class Helpers < ActiveRecord::Base
 
 #current user
+#accept session hash as an argument and use the user id from the session hash to find the user in the database and return that user. 
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
   end
