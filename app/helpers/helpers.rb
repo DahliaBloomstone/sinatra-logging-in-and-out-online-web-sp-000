fedc1e1f2eaaa060257d68388b41f7f68ca566d0
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-07-04 19:47:00 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,8 +1,11 @@
 class Helpers < ActiveRecord::Base
+
+#current user
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
   end
 
+#is logged in?
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
